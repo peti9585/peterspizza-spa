437600015153f7fa55323ae74c24563d9347de53
peti9585 <peti9585@gmail.com>
2025-06-26 18:59:30 +0200

    initial commit 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
 import {CommonModule} from '@angular/common';
-import {IntroductionComponent} from '../introduction/introduction.component';
-import {AboutComponent} from '../about/about.component';
-import {DescriptionComponent} from '../description/description.component';
-import {ComponentType} from '../description/description.component';
-import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    HeaderComponent,
-    CommonModule,
-    IntroductionComponent,
-    AboutComponent,
-    DescriptionComponent,
-    FooterComponent],
+  imports: [RouterOutlet, CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
-import {MainComponent} from '../main/main.component';
+import {MainComponent} from '../main-page/main/main.component';
+import {OrderMainComponent} from '../order-page/order-main/order-main.component';
+import {NotfoundComponent} from '../notfound/notfound.component';
+import {CartMainComponent} from '../cart-page/cart-main/cart-main.component';
+import {
+  PersonalDetailsMainComponent
+} from '../personal-details-page/personal-details-main/personal-details-main.component';
+import {RegistrationComponent} from '../registration-page/registration/registration.component';
+import {LoginComponent} from '../login-page/login/login.component';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: MainComponent
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  }
+  { path: 'home', component: MainComponent },
+  { path: 'order', component: OrderMainComponent },
+  { path: 'cart', component: CartMainComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: 'order-confirmation', component: PersonalDetailsMainComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404' }
 ];
