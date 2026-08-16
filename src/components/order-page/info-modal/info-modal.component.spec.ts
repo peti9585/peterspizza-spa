5c8e467b542ae2a30ba7c34c96f95022c435e4e5
peti9585 <peti9585@gmail.com>
2026-08-16 17:12:37 +0200

    Add unit-test for info modal component

--- src/components/order-page/info-modal/info-modal.component.spec.ts
+++ src/components/order-page/info-modal/info-modal.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfoModalComponent } from './info-modal.component';
+import {CookieService} from 'ngx-cookie-service';
 
 describe('InfoModalComponent', () => {
   let component: InfoModalComponent;
   let fixture: ComponentFixture<InfoModalComponent>;
 
+  const cookieServiceMock = {
+    set: vi.fn().mockName('set')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InfoModalComponent]
+      imports: [InfoModalComponent],
+      providers: [
+        { provide: CookieService, useValue: cookieServiceMock },
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(InfoModalComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    vi.clearAllMocks();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set cookie when button is clicked', () => {
+    // Act + Assert
+    fixture.detectChanges();
+    component.onClick();
+
+    expect(cookieServiceMock.set).toHaveBeenCalledWith('infoModalClosed', 'true', 0.0208);
   });
 });
