5cc527d52a1e48d2556af8e6e1bc92e3b1a9f192
peti9585 <peti9585@gmail.com>
2026-08-18 15:10:03 +0200

    Add more unit-tests

--- src/components/profile-page/profile-data/profile-data.component.spec.ts
+++ src/components/profile-page/profile-data/profile-data.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { vi } from 'vitest';
-import { of } from 'rxjs';
+import {of, throwError} from 'rxjs';
 import { ProfileDataComponent } from './profile-data.component';
 import { UserService } from '../../../services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { IGetUserDetailsByIdResponse } from '../../../interfaces/interfaces-global';
+import {IGetUserDetailsByIdResponse, IUpdateUserRequest} from '../../../interfaces/interfaces-global';
 
 describe('ProfileDataComponent', () => {
   let component: ProfileDataComponent;
@@ -38,10 +38,78 @@ describe('ProfileDataComponent', () => {
 
     fixture = TestBed.createComponent(ProfileDataComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    vi.clearAllMocks();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a form with default values', () => {
+    // Act + Assert
+    const form = component.formGroup;
+
+    expect(form).toBeTruthy();
+
+    expect(form.get('firstName')).toBeTruthy();
+    expect(form.get('lastName')).toBeTruthy();
+    expect(form.get('phoneNumber')).toBeTruthy();
+    expect(form.get('email')).toBeTruthy();
+
+    expect(form.get('firstName')?.value).toBe('');
+    expect(form.get('lastName')?.value).toBe('');
+    expect(form.get('phoneNumber')?.value).toBe('');
+    expect(form.get('email')?.value).toBe('');
+  });
+
+  it('should fetch user data and fill the form and set isLoading flag to false', () => {
+    // Arrange
+    const form = component.formGroup;
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(form.get('firstName')?.value).toBe('John');
+    expect(form.get('lastName')?.value).toBe('Doe');
+    expect(form.get('phoneNumber')?.value).toBe('+36201234567');
+    expect(form.get('email')?.value).toBe('john.doe@example.com');
+
+    expect(component.isLoading).toBeFalsy();
+  });
+
+  it('should open a toaster in case of error during initial fetch', () => {
+    // Arrange
+    const form = component.formGroup;
+    userServiceMock.getUserDetailsById.mockReturnValueOnce(throwError(() => new Error('Error')));
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(form.get('firstName')?.value).toBe('');
+    expect(form.get('lastName')?.value).toBe('');
+    expect(form.get('phoneNumber')?.value).toBe('');
+    expect(form.get('email')?.value).toBe('');
+
+    expect(toasterMock.open).toHaveBeenCalled();
+  });
+
+  it('should send user update request based on form values', () => {
+    // Arrange
+    const expectedRequest: IUpdateUserRequest = {
+      firstName: 'John',
+      lastName: 'Doe',
+      phoneNumber: '+36201234567',
+      email: 'john.doe@example.com'
+    };
+
+    // Act
+    fixture.detectChanges();
+    component.sendUpdateRequest();
+
+    // Assert
+    expect(userServiceMock.updateUser).toHaveBeenCalledWith(expectedRequest);
+    expect(toasterMock.open).toHaveBeenCalled();
   });
 });
